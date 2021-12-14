8da6dd4936cac1111219016b09d12fa8d2f0a185
Karanpreet Singh <karanpreetsingh1990@gmail.com>
2021-12-15 01:00:49 +0530

    Search endpoint functional with support for regex and multiple filters.

--- apiServer.py
+++ apiServer.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from collections import defaultdict
+from flask import Flask, jsonify, request
 import logging
 from logging.config import fileConfig
 from configparser import ConfigParser
@@ -7,6 +8,8 @@ import sys
 import csv
 from flask_compress import Compress
 from waitress import serve
+import re
+from pprint import pprint
 
 
 app = Flask(__name__)
@@ -69,9 +72,57 @@ def getData(csvFile):
         return {'error': 'File not found'}, 404
 
 
+@app.route('/<csvFile>/search')
+def searchData(csvFile):
+    if len(list(request.args.keys())) >= 1:
+        paramName = []
+        paramValue = []
+        for params in request.args.keys():
+            paramName.append(params.lower())
+            paramValue.append(request.args.get(params).lower())
+        if parser.has_section(csvFile):
+            section = csvFile
+            logger.debug('Explicit config section found for {}'.format(csvFile))
+        else:
+            section = 'Global'
+            logger.debug('No section found for {}. Using Global config'.format(csvFile))
+        if os.path.exists(os.path.join(parser.get(section, 'csv_path'), csvFile + '.csv')):
+            logger.debug('CSV file found in the path.')
+            headers = []
+            jsonData = defaultdict(list)
+            with open(os.path.join(parser.get(section, 'csv_path'), csvFile + '.csv'), 'r') as infile:
+                csvData = csv.reader(infile)
+                for index, line in enumerate(csvData):
+                    if index == 0:
+                        if parser.has_option(section, 'headers'):
+                            logger.debug('Headers found in configuration to be used as JSON keys. Ignoring the first line and using this instead.')
+                            headers = parser.get(section, 'headers').split(',')
+                        else:
+                            logger.debug('Using the first line as headers')
+                            headers = line
+                    else:
+                        status = 0
+                        for lineIndex, column in enumerate(line):
+                            if headers[lineIndex].replace('"', '').strip().lower() in paramName:
+                                if column.replace('"', '').strip().lower() in paramValue[paramName.index(headers[lineIndex].replace('"', '').lower().strip())] or re.search(paramValue[paramName.index(headers[lineIndex].replace('"', '').strip().lower())], column, re.IGNORECASE):
+                                    status += 1
+                        if status == len(paramName):
+                            jsonData['result'].append({str(headers[index].replace('"', '').strip()): str(column.replace('"', '').strip()) for index, column in enumerate(line)})
+            jsonData['count'] = len(jsonData['result'])
+            return jsonify(jsonData), 200
+        else:
+            logger.error('File Not found in path.')
+            return {'error': 'File not found'}, 404
+    else:
+        logger.error('Did not find any search parameters')
+        return {'error': 'No search parameter defined with search endpoint.'}, 500
+
+
 if __name__ == '__main__':
     parser = readConfig('init.conf')
     logger = configureLogging('logging_config.ini')
     app.secret_key = os.urandom(16)
     # app.run(debug=True, port=5555, host='0.0.0.0')
+    # app.debug = True
+    # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     serve(app, port=parser.get('Global', 'Port'), host='0.0.0.0', url_scheme='https')
